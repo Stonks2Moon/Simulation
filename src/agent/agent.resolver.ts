fe35b9c94b927714ce3ccc8eef02bae0a6e69d08
Vonend, Matthias <matthias.vonend@outlook.de>
2021-02-18 13:22:37 +0100

    graphql subscription test

--- src/agent/agent.resolver.ts
+++ src/agent/agent.resolver.ts
@@ -1,10 +1,34 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Int,
+  Mutation,
+  Query,
+  Resolver,
+  Subscription,
+} from '@nestjs/graphql';
 import { Agent } from './agent.dto';
+import { PubSub } from 'graphql-subscriptions';
+import {  GraphQLString } from 'graphql';
+
+const pubSub = new PubSub();
 
 @Resolver((_) => Agent)
 export class AgentResolver {
-  @Query((_) => Agent)
-  async author(@Args('id', { type: () => Int }) id: number) {
-    return new Agent();
+  @Query((_) => [Agent])
+  async getAgent(
+    @Args('id', { type: () => [Int], nullable: true }) ids: number[],
+  ) {
+    return [new Agent()];
+  }
+
+  @Subscription(_ => GraphQLString)
+  commentAdded() {
+    return pubSub.asyncIterator('commentAdded');
+  }
+
+  @Mutation(_ => GraphQLString)
+  async addComment() {
+    pubSub.publish('commentAdded', { commentAdded: 'test' });
+    return 'test';
   }
 }
