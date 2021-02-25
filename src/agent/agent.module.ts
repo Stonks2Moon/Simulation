8d86299b9997baf94f10aaf68ac11685f0aba70e
Vonend, Matthias <matthias.vonend@outlook.de>
2021-02-25 17:15:43 +0100

    no message

--- src/agent/agent.module.ts
+++ src/agent/agent.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AgentService } from './services/agent.service';
 import { BrainService } from './services/brain.service';
-import { MarketController } from '../market/market.controller';
 import { MarketModule } from '../market/market.module';
 
 @Module({
   imports: [MarketModule],
   providers: [AgentService, BrainService],
-  controllers: [MarketController],
 })
 export class AgentModule {}
