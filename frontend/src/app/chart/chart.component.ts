0c018c0b1e57b4537151e0d87ddbc5313b7d836d
Troy Keßler <troy.kessler99@gmail.com>
2021-03-04 10:59:36 +0100

    added boxmueller to frontend

--- frontend/src/app/chart/chart.component.ts
+++ frontend/src/app/chart/chart.component.ts
@@ -13,17 +13,7 @@ import { graphic } from 'echarts';
 let now = new Date(1997, 9, 3);
 const oneDay = 24 * 3600 * 1000;
 let value = Math.random() * 1000;
-function randomData() {
-  now = new Date(+now + oneDay);
-  value = value + Math.random() * 21 - 10;
-  return {
-    name: now.toString(),
-    value: [
-      [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-      Math.round(value),
-    ],
-  };
-}
+
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
@@ -31,6 +21,72 @@ function randomData() {
 })
 export class ChartComponent implements OnInit {
   private data: any[] = [];
+
+  private nextPrice: any;
+
+  private x1: any;
+  private x2: any;
+  private w: any;
+  private z: any;
+  private value: any = 0;
+  private points: any = [];
+  private t: any;
+
+  boxMuller(r: any[]) {
+    let phase = 0;
+    if (!phase) {
+      this.x1 = 2.0 * r[0] - 1.0;
+      this.x2 = 2.0 * r[1] - 1.0;
+      this.w = this.x1 * this.x1 + this.x2 * this.x2;
+      if (this.w >= 1.0) {
+        this.w = Math.sqrt((-2.0 * Math.log(this.w)) / this.w);
+        this.z = this.x1 * this.w;
+      } else {
+        this.z = this.x2 * this.w;
+      }
+    } else {
+      this.z = this.x2 * this.w;
+    }
+
+    if (this.z == this.z) {
+      this.value += this.z;
+      this.points.push([this.t, this.value]);
+      this.points.map((point: any) => {
+        this.calculations(point[1]);
+        point = point.slice(-1);
+      });
+      this.points = this.points.slice(-1);
+    }
+    phase ^= 1;
+  }
+
+  calculations(result: number) {
+    const base = 0;
+    const scale = 30;
+
+    if (base != 0) {
+      result = base + result * (base / scale);
+    }
+
+    this.nextPrice = result;
+  }
+
+  generateNextPrice(): number {
+    this.boxMuller([Math.random(), Math.random()]);
+    return this.nextPrice + 30;
+  }
+
+  randomData() {
+    now = new Date(+now + oneDay);
+    value = this.generateNextPrice()
+    return {
+      name: now.toString(),
+      value: [
+        [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+        value,
+      ],
+    };
+  }
 
   chartOptions: EChartsOption = {
     tooltip: {
@@ -120,13 +176,13 @@ export class ChartComponent implements OnInit {
 
   ngOnInit(): void {
     for (var i = 0; i < 1000; i++) {
-      this.data.push(randomData());
+      this.data.push(this.randomData());
     }
 
     setInterval(() => {
       for (var i = 0; i < 5; i++) {
         this.data.shift();
-        this.data.push(randomData());
+        this.data.push(this.randomData());
       }
 
       // this.chartOptions.series[0].data = this.data;
