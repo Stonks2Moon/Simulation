1304623aa9ab6d32394290687b4778ec4847efdd
Aaron Schweig <aaron.schweig@gmail.com>
2021-03-04 17:36:20 +0100

    Refactores tests and complies to mocking principle

--- backend/src/news/news.service.spec.ts
+++ backend/src/news/news.service.spec.ts
@@ -1,18 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
+import * as fs from 'fs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+jest.mock('slimbot');
+jest.mock('fs');
+
+const CHAT_ID = '-579864936';
 
 describe('NewsService', () => {
   let service: NewsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       providers: [NewsService],
-    }).compile();
+    })
+      .overrideProvider(ConfigService)
+      .useValue({
+        get: (key: string): string => {
+          switch (key) {
+            case 'TELEGRAM_BOT_TOKEN':
+              return 'BOT_TOKEN';
+            case 'TELEGRAM_GROUP_ID':
+              return CHAT_ID;
+            default:
+              break;
+          }
+        },
+      })
+      .compile();
 
     service = module.get<NewsService>(NewsService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should send a text message to a telegram group', () => {
+    const slimbot = service['slimbot'];
+    const slimSpy = jest.spyOn(slimbot, 'sendMessage');
+
+    service.sendNews('Test Message');
+
+    expect(slimSpy).toHaveBeenCalledTimes(1);
+    expect(slimSpy).toHaveBeenCalledWith(CHAT_ID, 'Test Message');
+  });
+
+  it('should send the stonks photo in a telegram group', () => {
+    const slimbot = service['slimbot'];
+    const slimSpy = jest.spyOn(slimbot, 'sendPhoto');
+
+    service.stonks();
+
+    expect(fs.createReadStream).toHaveBeenCalledTimes(1);
+    expect(slimSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should send the notStonks photo in a telegram group', () => {
+    const slimbot = service['slimbot'];
+    const slimSpy = jest.spyOn(slimbot, 'sendPhoto');
+
+    service.notStonks();
+
+    expect(fs.createReadStream).toHaveBeenCalledTimes(1);
+    expect(slimSpy).toHaveBeenCalledTimes(1);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
--- backend/src/news/news.service.ts
+++ backend/src/news/news.service.ts
@@ -1,25 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { join } from 'path';
 import * as Slimbot from 'slimbot';
-import { createReadStream } from 'fs';
+import { createReadStream, ReadStream } from 'fs';
+import { ConfigService } from '@nestjs/config';
 
-const slimbot = new Slimbot('1655303947:AAHNNqcRUt9qZ6q0fVtC7HVU11_JNu93-Bg');
+interface ISlimbot {
+  sendMessage(code: string, message: string): Promise<any>;
+  sendPhoto(code: string, file: ReadStream): Promise<any>;
+}
 
 @Injectable()
 export class NewsService {
+  private slimbot: ISlimbot;
+  private chatId: string;
+
+  constructor(configService: ConfigService) {
+    this.slimbot = new Slimbot(
+      configService.get<string>('TELEGRAM_BOT_TOKEN'),
+    );
+    this.chatId = configService.get<string>("TELEGRAM_GROUP_ID")
+  }
+
   public sendNews(message: string) {
-    return slimbot.sendMessage('-579864936', message);
+    return this.slimbot.sendMessage(this.chatId, message);
   }
 
   public stonks() {
-    const stonksFile = createReadStream(join(__dirname, '../assets/stonks.jpg'));
-    return slimbot.sendPhoto('-579864936', stonksFile);
+    const stonksFile = createReadStream(
+      join(__dirname, '../assets/stonks.jpg'),
+    );
+    return this.slimbot.sendPhoto(this.chatId, stonksFile);
   }
 
   public notStonks() {
     const notStonksFile = createReadStream(
       join(__dirname, '../assets/notstonks.jpg'),
     );
-    return slimbot.sendPhoto('-579864936', notStonksFile);
+    return this.slimbot.sendPhoto(this.chatId, notStonksFile);
   }
 }
