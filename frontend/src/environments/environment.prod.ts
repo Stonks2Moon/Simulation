ea66ba90cdb15159fae7c4fced064cae2004dbb7
Vonend, Matthias <matthias.vonend@outlook.de>
2021-04-16 10:19:53 +0200

    changed addresses

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   simulation_url: 'https://simulation.moonstonks.space/api/',
-  boerse_url: 'http://10.58.239.19:16001/'
+  boerse_url: 'https://boerse.moonstonks.space/'
 };
